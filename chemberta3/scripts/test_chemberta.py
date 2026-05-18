cb10ddb0bfb7693f3d20707775ea26d95b2e0ea6
priyadp1 <prishapriya31@gmail.com>
2026-05-18 15:28:00 -0400

    Added POC for token masking in chemberta3 and added rdkit for depndencies

---
+++ chemberta3/scripts/test_chemberta.py
@@ -0,0 +1,56 @@
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+import torch
+from rdkit import Chem
+
+#Load Model
+model_name = "DeepChem/ChemBERTa-77M-MTR"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForMaskedLM.from_pretrained(model_name)
+
+#Valid Tokens
+valid_tokens = ["C" , "N" , "O" , "S" , "P" , "F" , "C1" , "Br" , "=" , "#"]
+
+#Tokenize Smiles
+smiles = "CCO"
+tokens = tokenizer.tokenize(smiles)
+print("\nTokens:")
+print(tokens)
+
+#Mask Smiles
+tokens[1] = tokenizer.mask_token
+masked_smiles = tokenizer.convert_tokens_to_string(tokens)
+print("\nMasked Smiles")
+print(masked_smiles)
+inputs = tokenizer(masked_smiles, return_tensors="pt")
+
+#Run Inference
+with torch.no_grad():
+    outputs = model(**inputs)
+
+logits = outputs.logits
+
+#Extract top k predictions
+mask_token_index = torch.where(inputs["input_ids"] == tokenizer.mask_token_id)[1]
+mask_logits = logits[0, mask_token_index, :]
+
+top_k_preds = torch.topk(mask_logits,50,dim=1).indices[0].tolist()
+
+print("\nTop Predictions:")
+candidate_smiles_list = []
+for i in top_k_preds:
+    predicted_token = tokenizer.decode([i]).strip()
+    if predicted_token not in valid_tokens:
+        continue
+    print(predicted_token)
+    candidate_smiles = masked_smiles.replace(tokenizer.mask_token,predicted_token)
+    candidate_smiles_list.append(candidate_smiles)
+
+#Validate using RDKit
+print("\nValidation Results:")
+for candidate_smiles in candidate_smiles_list:
+    molecule = Chem.MolFromSmiles(candidate_smiles)
+    if molecule is not None:
+        print(f"{candidate_smiles}: Valid Molecule!")
+    else:
+        print(f"{candidate_smiles}: Invalid Molecule!")
